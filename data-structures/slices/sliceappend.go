864f5ebd46ff20520bfb49716daffb6f29a1a5de
Daniel McMahon <daniel.mcmahon@zalando.ie>
2017-10-09 16:16:40 +0100

    Further programs/exercises completed

---
+++ data-structures/slices/sliceappend.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	var s []int
+	printSlice(s)
+
+	//append works on nil slices
+	s = append(s, 0)
+	printSlice(s)
+
+	//the slice grows as needed
+	s = append(s, 1)
+	printSlice(s)
+
+	//we can add more than one elemnet at a time
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+
+}
